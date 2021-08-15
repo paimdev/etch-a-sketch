29edaaf8f3188625e953979b3999ad2f585fac30
paimdev <bathkelucas@gmail.com>
2021-08-15 15:40:06 -0300

    Add the loop for creating the sketchDivs

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+let size = 16;
+
 const container = document.querySelector(".container");
-container.style.cssText = "grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16,1fr)";
+container.style.cssText = `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size},1fr)`;
+
+for (let i=0; i<(size*size); i++) {
+  const sketchDiv = document.createElement('div');
+  container.appendChild(sketchDiv);
+  console.log(i);
+}
