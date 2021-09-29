b3bbfc8f1b79a0d1b8547a1555b97935ca65cbc3
paimdev <bathkelucas@gmail.com>
2021-09-29 09:23:50 -0300

    Fix grid factory and add event listener to items

--- main.js
+++ main.js
@@ -7,17 +7,12 @@ function gridSize(rows, cols) {
   for (let i=0; i<(rows*cols); i++) {
     let sketchDiv = document.createElement('div');
     container.appendChild(sketchDiv).className = "grid-item";
-    sketchDiv.classList.add(`item-${i}`);
-    console.log(i);
+    sketchDiv.addEventListener('mouseover', event => {
+      sketchDiv.classList.add('painted-item')
+    });
   };
 };
 
-const gridElements = document.querySelectorAll(".grid-item");
 
-for (const element of gridElements) {
-  element.addEventListener("mouseover", event => {
-    element.classList.add('painted-item');
-  });
-}
 
 gridSize(18, 18);
