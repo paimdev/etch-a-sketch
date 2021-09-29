65b1ce8ae162fd2311fff29728c281b5f02d7fe4
paimdev <bathkelucas@gmail.com>
2021-09-29 10:17:05 -0300

    Add function to reset button

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const container = document.querySelector('.container');
-const button = document.querySelector('.reset-button')
+const button = document.querySelector('.reset-button');
 
 function gridSize(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
-
+  
   for (let i=0; i<(rows*cols); i++) {
     let sketchDiv = document.createElement('div');
     container.appendChild(sketchDiv).className = "grid-item";
@@ -14,10 +14,29 @@ function gridSize(rows, cols) {
   };
 };
 
+function clearGrid() {
+  const items = document.querySelectorAll('.grid-item');
+  for (const item of items) {
+    item.remove();
+  };
+};
+
+function askPrompt() {
+  var size = prompt('What size should the grid be?');
+  if (size > 100){
+    alert('The max amount is 100 items!');
+    askPrompt();
+  } else {
+    gridSize(size, size);
+  };
+};
+
 button.addEventListener('click', event => {
-  console.log('this button was clicked')
+  console.log('grid cleared!');
+  clearGrid();
+  askPrompt();
 });
 
 
 
-gridSize(18, 18);
+gridSize(16, 16);
